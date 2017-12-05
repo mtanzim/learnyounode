1f5ac82ecf46e62db100d6653e09325995a633cc
Tanzim Mokammel <mtanzim@gmail.com>
2017-12-05 15:45:41 +0000

    Completed learnyounode :D :D :D

--- httpJSON.js
+++ httpJSON.js
@@ -5,6 +5,31 @@ var port=process.argv[2];
 
 
 //console.log(port);
+function convertTimeformat (reqUrl, isUnix) {
+    let time,h,m,s, timeObj;
+
+    
+    if(isUnix){
+        //var parsedUnixTime= new Date('Mon, 25 Dec 1995 13:30:00 GMT').getUnixTime();
+        time=reqUrl.path.match(/(=.*)/g).join('');
+        time=time.substring(1, time.length-1);
+        time=new Date(time).getTime();
+        timeObj = {'unixtime': time};
+        //console.log(new Date(time).getTime());
+        return JSON.stringify(timeObj);
+        //return 'Hi';
+    } else {
+        time=reqUrl.path.match(/(T[^a-zA-Z]*\.)/g).join('');
+        time=time.substring(1, time.length-1);
+        h=parseInt(time.split(':')[0]);
+        m=parseInt(time.split(':')[1]);
+        s=parseInt(time.split(':')[2]);
+        timeObj = {'hour': h, 'minute':m, 'second':s};
+        return JSON.stringify(timeObj);
+    }
+    
+}
+
 
 var server=http.createServer(function(req,res){
     var reqUrl=url.parse(req.url, true);
@@ -12,18 +37,11 @@ var server=http.createServer(function(req,res){
     
     if (req.url.includes('parsetime')){
         //console.log(reqUrl.path);
-        let time,h,m,s, timeObj;
-        time=reqUrl.path.match(/(T[^a-zA-Z]*\.)/g).join('');
-        time=time.substring(1, time.length-1);
-        h=parseInt(time.split(':')[0]);
-        m=parseInt(time.split(':')[1]);
-        s=parseInt(time.split(':')[2]);
-        timeObj = {'hour': h, 'minute':m, 'second':s};
-        //console.log(JSON.stringify(timeObj));
-        toWrite=JSON.stringify(timeObj);
+        toWrite=convertTimeformat(reqUrl, false);
     } else if (req.url.includes('unixtime')) {
         //console.log('need unixtime');
-        toWrite=reqUrl.pathname
+        //toWrite=reqUrl.pathname;
+        toWrite=convertTimeformat(reqUrl, true);
     } else {
         toWrite='Bad pathname provided'
     }
